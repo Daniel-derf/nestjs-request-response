143fc308edae5fb8bd20b8c225c5231e7fd9ab8e
Daniel-derf <danielpardinho321@gmail.com>
2024-12-02 05:44:03 -0300

    created pipe

---
+++ src/pipes/freeze.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  Logger,
+} from '@nestjs/common';
+
+@Injectable()
+export class FreezePipe implements PipeTransform {
+  private readonly logger = new Logger(FreezePipe.name);
+
+  transform(value: any) {
+    this.logger.debug('FreezePipe running...');
+    Object.freeze(value);
+    return value;
+  }
+}
